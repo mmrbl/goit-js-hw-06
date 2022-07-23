695204a2dcf8f20be40da9e3eb3195f00ba8e6a8
Ihor <itotonk1@gmail.com>
2022-07-23 20:50:35 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
-const itemAmount = document.querySelectorAll(".item");
-console.log(`Number of categories: ${itemAmount.length}`);
+const countItems = document.querySelectorAll(".item").length;
+console.log(`Number of categories: ${countItems}`);
 
-const categories = itemAmount;
+const categories = document.querySelectorAll(".item");
 categories.forEach((category) => {
   const title = category.querySelector("h2").textContent;
   const count = category.querySelectorAll("li").length;
