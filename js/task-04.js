af28f921af247051b5882cc07745eb5f5d5eb4d1
Ihor <itotonk1@gmail.com>
2022-07-23 16:55:39 +0300

    Додає другу таску

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+// 1) Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
+// 2) Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
+// 3) Оновлюй інтерфейс новим значенням змінної counterValue.
+
+const decrement = document.querySelector(`[data-action="decrement"]`);
+const increment = document.querySelector(`[data-action="increment"]`);
+const counter = document.querySelector(`#value`);
+
+let counterValue = 0;
+
+const decrementFn = () => {
+  counterValue -= 1;
+  counter.textContent = counterValue;
+};
+decrement.addEventListener("click", decrementFn);
+
+const incrementFn = () => {
+  counterValue += 1;
+  counter.textContent = counterValue;
+};
+increment.addEventListener("click", incrementFn);
