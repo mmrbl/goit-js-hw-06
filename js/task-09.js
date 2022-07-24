620c0357e7df6b1f2d8029a913c5b6792f115c4a
Ihor <itotonk1@gmail.com>
2022-07-24 17:33:22 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const btn = document.querySelector('.change-color');
+const hexColor = document.querySelector('.color');
+
+const changeColor = () => {
+  const newColor = getRandomHexColor();
+  hexColor.textContent = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+};
+
+btn.addEventListener('click', changeColor);
