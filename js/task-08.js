dd045d172c955754d40d2f7add1a65dfc3a02423
Ihor <itotonk1@gmail.com>
2022-07-24 15:18:22 +0300

    Додає восьму таску

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+const formValidation = event => {
+  event.preventDefault();
+
+  if (
+    event.currentTarget.elements.email.value === '' ||
+    event.currentTarget.elements.password.value === ''
+  ) {
+    return alert('Для завершення реєстрації, заповни усі поля.');
+  }
+
+  console.log(
+    `${event.currentTarget.elements.email.name}: ${event.currentTarget.elements.email.value}, ${event.currentTarget.elements.password.name}: ${event.currentTarget.elements.password.value} `
+  );
+
+  event.currentTarget.reset();
+};
+
+form.addEventListener('submit', formValidation);
